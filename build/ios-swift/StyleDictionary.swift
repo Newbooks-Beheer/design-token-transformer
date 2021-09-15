fdbced0c287b154b7d4a8ff37591de0e87c82886
Automated Publisher <actions@users.noreply.github.com>
2021-09-15 11:40:14 +0000

    Automated publish: Wed Sep 15 11:40:14 UTC 2021 395fcf6b1878bce5efec1f3634e9957a7d6d9b95

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 15 Sep 2021 08:19:24 GMT
+// Generated on Wed, 15 Sep 2021 11:40:13 GMT
 
 
 import UIKit
--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -3,7 +3,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 15 Sep 2021 08:19:24 GMT
+// Generated on Wed, 15 Sep 2021 11:40:13 GMT
 
 
 #import <Foundation/Foundation.h>
